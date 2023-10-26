d46d302ef3d67633294450d4ca34c11cb217e146
Vanronyco-Malau <Vanronyco-Malau.vanronyco17@gmail.com>
2023-10-26 22:26:06 +0700

    Membuat halaman katalog buku, Membuat halaman Detail buku, Membuat halaman controller member, Membuat halaman daftar anggota dengan modal, Membuat halaman login anggota dengan modal, Memodifikasi data anggota,Membuat sebuah controller booking dan pinjam, Memodifikasi controller booking dan pinjam, Memodifikasi halaman booking buku

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,58 +1,58 @@
-<?php 
-
-class Home extends CI_Controller {
+<?php
+class Home extends CI_Controller
+{
     function __construct()
     {
         parent::__construct();
     }
-    public function index(){
+    public function index()
+    {
         $data = [
             'judul' => "Katalog Buku",
             'buku' => $this->ModelBuku->getBuku()->result(),
         ];
 
-        // jika sudah login dan jika belum login
-        if ($this->session->userdata('email')){
+        //jika sudah login dan jika belum login
+        if ($this->session->userdata('email')) {
             $user = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 
             $data['user'] = $user['nama'];
 
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
-            $this->load->view('templates/templates-user/modal');
+            $this->load->view('templates/templates-user/modal',);
             $this->load->view('templates/templates-user/footer', $data);
-        }
-        else{
+        } else {
             $data['user'] = 'Pengunjung';
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
-            $this->load->view('templates/templates-user/modal');
+            $this->load->view('templates/templates-user/modal',);
             $this->load->view('templates/templates-user/footer', $data);
         }
+        //membuat home.php
     }
-    public function detailBuku() { 
-        $id = $this->uri->segment(3); 
-        $buku = $this->ModelBuku->joinKategoriBuku(['buku.id' => $id])->result(); 
-
-        $data['user'] = "Pengunjung"; 
-        $data['title'] = "Detail Buku"; 
-        
-        foreach ($buku as $fields) { 
-            $data['judul'] = $fields->judul_buku; 
-            $data['pengarang'] = $fields->pengarang; 
-            $data['penerbit'] = $fields->penerbit; 
-            $data['kategori'] = $fields->kategori; 
-            $data['tahun'] = $fields->tahun_terbit; 
-            $data['isbn'] = $fields->isbn; 
-            $data['gambar'] = $fields->image; 
-            $data['dipinjam'] = $fields->dipinjam; 
-            $data['dibooking'] = $fields->dibooking; 
-            $data['stok'] = $fields->stok; 
-            $data['id'] = $id; 
-        } 
-        $this->load->view('templates/templates-user/header', $data); 
+    public function detailBuku()
+    {
+        $id = $this->uri->segment(3);
+        $buku = $this->ModelBuku->joinKategoriBuku(['buku.id' => $id])->result();
+        $data['user'] = "Pengunjung";
+        $data['title'] = "Detail Buku";
+        foreach ($buku as $fields) {
+            $data['judul'] = $fields->judul_buku;
+            $data['pengarang'] = $fields->pengarang;
+            $data['penerbit'] = $fields->penerbit;
+            $data['kategori'] = $fields->kategori;
+            $data['tahun'] = $fields->tahun_terbit;
+            $data['isbn'] = $fields->isbn;
+            $data['gambar'] = $fields->image;
+            $data['dipinjam'] = $fields->dipinjam;
+            $data['dibooking'] = $fields->dibooking;
+            $data['stok'] = $fields->stok;
+            $data['id'] = $id;
+        }
+        $this->load->view('templates/templates-user/header', $data);
         $this->load->view('buku/detail-buku', $data);
-        $this->load->view('templates/templates-user/modal');
-        $this->load->view('templates/templates-user/footer'); 
+        $this->load->view('templates/templates-user/modal',);
+        $this->load->view('templates/templates-user/footer');
     }
 }
